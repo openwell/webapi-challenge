89c7c2ec10d2abaf68be29956f7244e685c678b8
openwell <timileyinojo@yahoo.com>
2019-07-12 11:34:34 +0100

    added route for adding an action

--- src/actions/actionsRoutes.js
+++ src/actions/actionsRoutes.js
@@ -4,7 +4,33 @@ const db = require("../../data/helpers/actionModel");
 const route = express.Router();
 
 route.post("/", (req, res) => {
-  
+  const { project_id, description, notes, completed } = req.body;
+  if (!req.body) {
+    return res.status(400).json({
+      message: "missing project data"
+    });
+  } else if (!project_id || !description || !notes || !completed) {
+    return res.status(400).json({
+      message: "invalid project_id/description/notes/completed"
+    });
+  }
+  try {
+    db.get(project_id).then(data => {
+      if (data.length >= 1) {
+        db.insert(req.body).then(data => {
+          return res.status(200).json({
+            data: data
+          });
+        });
+      } else {
+        return res.status(400).json({
+          message: "Invalid Project_id"
+        });
+      }
+    });
+  } catch (err) {
+    res.status(500).send(err);
+  }
 });
 
 route.get("/", (req, res) => {
