3af51a51e601781732afea2212edae08296dced5
openwell <timileyinojo@yahoo.com>
2019-07-12 10:27:58 +0100

    added route for fetching all projects

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const helmet = require("helmet");
 
 const actionsRoute = require('./actions/actionsRoutes')
+const projectsRoute = require('./products/projectsRoute')
 
 const app = express();
 app.use(express.json());
@@ -9,6 +10,7 @@ app.use(logger, helmet());
 
 
 app.use('/api/actions', actionsRoute)
+app.use('/api/projects', projectsRoute)
 
 
 app.get("/", (req, res) => {
---
+++ src/products/projectsRoute.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const db = require("../../data/helpers/projectModel");
+
+const route = express.Router();
+
+route.post("/", (req, res) => {
+  
+});
+
+route.get("/", (req, res) => {
+  try {
+    db.get().then(data => {
+      res.status(200).json(data);
+    });
+  } catch (err) {
+    res.status(500).json({ err });
+  }
+});
+
+route.delete("/:id", (req, res) => {});
+
+route.put("/:id", (req, res) => {});
+
+module.exports = route;
+
+
+
+
