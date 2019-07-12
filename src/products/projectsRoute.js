85095fc5416e8dd54c98277feaf8550a007a647f
openwell <timileyinojo@yahoo.com>
2019-07-12 10:44:56 +0100

    added route for updating a project

--- src/products/projectsRoute.js
+++ src/products/projectsRoute.js
@@ -37,6 +37,36 @@ route.get("/", (req, res) => {
 
 route.delete("/:id", (req, res) => {});
 
-route.put("/:id", (req, res) => {});
+route.put("/:id", (req, res) => {
+  const { name, description } = req.body;
+  const { id } = req.params;
+  if (!req.body) {
+    return res.status(400).json({
+      message: "missing project data"
+    });
+  } else if (!name || !description) {
+    return res.status(400).json({
+      message: "invalid project name or description"
+    });
+  } else if (!id || isNaN(id)) {
+    return res.status(400).json({
+      message: "invalid id"
+    });
+  }
+  try {
+    db.update(id, req.body).then(data => {
+      if (data === null) {
+        return res.status(200).json({
+          message: "invalid id"
+        });
+      }
+      return res.status(200).json({
+        data: data
+      });
+    });
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
 
 module.exports = route;
